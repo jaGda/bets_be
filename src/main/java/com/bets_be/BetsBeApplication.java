03f83cd9a327a74f15b969242147d1b76391d784
jagda <danielzychniewicz@wp.pl>
2021-10-14 11:36:18 +0200

    Initial commit

---
+++ src/main/java/com/bets_be/BetsBeApplication.java
@@ -0,0 +1,13 @@
+package com.bets_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetsBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BetsBeApplication.class, args);
+    }
+
+}
