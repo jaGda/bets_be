ce360b2b9b2175c5d2b139ebf645deac9e74ab2b
jagda <danielzychniewicz@wp.pl>
2021-10-17 20:12:45 +0200

    Controllers have been updated

--- src/main/java/com/bets_be/controller/CouponController.java
+++ src/main/java/com/bets_be/controller/CouponController.java
@@ -2,63 +2,42 @@ package com.bets_be.controller;
 
 import com.bets_be.domain.CouponDto;
 import com.bets_be.mapper.CouponMapper;
-import com.bets_be.repository.CouponDao;
-import com.bets_be.repository.UserDao;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.bets_be.service.CouponService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/coupons")
 public class CouponController {
 
-    @Autowired
-    private CouponDao couponDao;
-
-    @Autowired
-    private CouponMapper couponMapper;
-
-    @Autowired
-    private UserDao userDao;
+    private final CouponService service;
+    private final CouponMapper mapper;
 
     @GetMapping
     public List<CouponDto> getCoupons() {
-        return couponMapper.mapToCouponDtoList(couponDao.findAll());
+        return mapper.mapToCouponDtoList(service.getAllCoupons());
     }
 
     @GetMapping("/{couponId}")
     public CouponDto getCoupon(@PathVariable Long couponId) {
-        return couponMapper.mapToCouponDto(couponDao.findById(couponId).orElseThrow(
-                () -> new RuntimeException("Coupon of id '" + couponId + "' doesn't exist")
-        ));
-    }
-
-    @GetMapping("/{userId}")
-    public List<CouponDto> getAllUserCoupons(@PathVariable Long userId) {
-        return couponMapper.mapToCouponDtoList(
-                couponDao.findAllByUser(userDao.findById(userId).orElseThrow(
-                        () -> new RuntimeException("User of id '" + userId + "' doesn't exist")
-                ))
-        );
+        return mapper.mapToCouponDto(service.getCouponById(couponId));
     }
 
     @PostMapping
     public CouponDto createCoupon(@RequestBody CouponDto couponDto) {
-        return couponMapper.mapToCouponDto(couponDao.save(couponMapper.mapToCoupon(couponDto)));
+        return mapper.mapToCouponDto(service.addCoupon(mapper.mapToCoupon(couponDto)));
     }
 
     @PutMapping
     public CouponDto updateCoupon(@RequestBody CouponDto couponDto) {
-        if (couponDao.findById(couponDto.getId()).isEmpty()) {
-            throw new RuntimeException("Coupon of id '" + couponDto.getId() + "' doesn't exist");
-        } else {
-            return couponMapper.mapToCouponDto(couponDao.save(couponMapper.mapToCoupon(couponDto)));
-        }
+        return mapper.mapToCouponDto(service.updateCoupon(mapper.mapToCoupon(couponDto)));
     }
 
     @DeleteMapping("/{couponId}")
     public void deleteCoupon(@PathVariable Long couponId) {
-        couponDao.deleteById(couponId);
+        service.removeCouponById(couponId);
     }
 }
--- src/main/java/com/bets_be/controller/UserController.java
+++ src/main/java/com/bets_be/controller/UserController.java
@@ -2,50 +2,42 @@ package com.bets_be.controller;
 
 import com.bets_be.domain.UserDto;
 import com.bets_be.mapper.UserMapper;
-import com.bets_be.repository.UserDao;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.bets_be.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/users")
 public class UserController {
 
-    @Autowired
-    private UserDao userDao;
-
-    @Autowired
-    private UserMapper userMapper;
+    private final UserService service;
+    private final UserMapper mapper;
 
     @GetMapping
     public List<UserDto> getUsers() {
-        return userMapper.mapToUserDtoList(userDao.findAll());
+        return mapper.mapToUserDtoList(service.getAllUsers());
     }
 
     @GetMapping("/{userId}")
     public UserDto getUser(@PathVariable Long userId) {
-        return userMapper.mapToUserDto(userDao.findById(userId).orElseThrow(
-                () -> new RuntimeException("User of id '" + userId + "' doesn't exist")
-        ));
+        return mapper.mapToUserDto(service.getUserById(userId));
     }
 
     @PostMapping
     public UserDto createUser(@RequestBody UserDto userDto) {
-        return userMapper.mapToUserDto(userDao.save(userMapper.mapToUser(userDto)));
+        return mapper.mapToUserDto(service.addUser(mapper.mapToUser(userDto)));
     }
 
     @PutMapping
     public UserDto updateUser(@RequestBody UserDto userDto) {
-        if (userDao.findById(userDto.getId()).isEmpty()) {
-            throw new RuntimeException("User of id '" + userDto.getId() + "' doesn't exist");
-        } else {
-            return userMapper.mapToUserDto(userDao.save(userMapper.mapToUser(userDto)));
-        }
+        return mapper.mapToUserDto(service.updateUser(mapper.mapToUser(userDto)));
     }
 
     @DeleteMapping("/{userId}")
     public void deleteUser(@PathVariable Long userId) {
-        userDao.deleteById(userId);
+        service.removeUserById(userId);
     }
 }
