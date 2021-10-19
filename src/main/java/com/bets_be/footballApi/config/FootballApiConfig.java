de13589d3904ae42f1a77d8b9755c6e484bfa144
jagda <danielzychniewicz@wp.pl>
2021-10-19 16:02:46 +0200

    FootballApi configuration class added

---
+++ src/main/java/com/bets_be/footballApi/config/FootballApiConfig.java
@@ -0,0 +1,25 @@
+package com.bets_be.footballApi.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Component
+public class FootballApiConfig {
+
+    @Value("${football.api.endpoint.fixtures.prod}")
+    private String footballApiEndpoint;
+
+    @Value("${football.api.host.key}")
+    private String headerHostKey;
+
+    @Value("${football.api.host.value}")
+    private String headerHostValue;
+
+    @Value("${football.api.key}")
+    private String footballApiKey;
+
+    @Value("${football.api.key.value}")
+    private String footballApiValue;
+}
