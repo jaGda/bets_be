95bb2994b48b4442632a761ae7e6b81d01a06431
jagda <danielzychniewicz@wp.pl>
2021-10-19 20:23:38 +0200

    FootballApiController updated

--- src/main/java/com/bets_be/controller/FootballApiController.java
+++ src/main/java/com/bets_be/controller/FootballApiController.java
@@ -1,21 +1,29 @@
 package com.bets_be.controller;
 
 import com.bets_be.domain.FixturesDto;
+import com.bets_be.domain.OddsDto;
 import com.bets_be.footballApi.client.FootballApiClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/fixtures")
+@RequestMapping
 public class FootballApiController {
 
     private final FootballApiClient footballApiClient;
 
-    @GetMapping
+    @GetMapping("/fixtures")
     public FixturesDto getFixtures() {
         return footballApiClient.getFixtures();
+    }
+
+    @GetMapping("/odds")
+    public OddsDto getOdds() {
+        return footballApiClient.getOdds(LocalDate.now().plusDays(4));
     }
 }
